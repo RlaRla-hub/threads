344f6cf4fb491f9b64e888e503bd666a35e9ea06
koshka_dusya <margalexboch@mail.ru>
2026-02-23 16:23:35 +0300

    Сделана архитектура класса стека. Реализованы методы pop(), push() с перегрузкой для rvalue, top(), empty(), size(), clear()

---
+++ thread/ThreadSafeStack.h
@@ -0,0 +1,75 @@
+/*Ѕезопасный стек : –еализуйте класс ThreadSafeStack(на основе std::vector или std::stack)
+с операци€ми push и pop.«ащитите внутренние данные мьютексом.pop должен корректно обрабатывать случай пустого стека
+*/
+
+//std::optional<T> top() const должен делать возврат по ссылке, чтобы не было лишнего копировани€
+//реализовать метод emplace_back() 
+//сделать возврат значени€ в операции pop() 
+//потокобезопасность 
+
+
+#pragma once
+
+#include <vector>
+#include <optional>
+
+template <typename T>
+class ThreadSafeStack
+{
+private:
+	std::vector<T> values;
+
+public:
+
+	void pop() noexcept
+	{
+		if(!values.empty())
+		{
+			values.pop_back();
+		}
+	}
+
+	void push(const T& value)
+	{
+		try
+		{
+			values.push_back(value);
+		}
+		catch(std::bad_alloc exp)
+		{
+			std::cerr << "—тек переполнен, недостаточно пам€ти " << exp.what() << "\n";
+		}
+	}
+
+	void push(T&& value)
+	{
+		try
+		{
+			values.push_back(std::move(value));
+		}
+		catch (std::bad_alloc exp)
+		{
+			std::cerr << "—тек переполнен, недостаточно пам€ти " << exp.what() << "\n";
+		}
+	}
+
+	std::optional<T> top() const 
+	{
+		return !values.empty() ? std::optional<T>(values.back()) : std::nullopt;
+	}
+
+	bool empty() const noexcept
+	{
+		return values.empty();
+	}
+
+	size_t size() const noexcept
+	{
+		return values.size();
+	}
+
+	void clear() noexcept
+	{
+		values.clear();
+	}
+};
---
+++ thread/thread.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "ThreadSafeStack.h"
+#include <thread>
+
+
+int main()
+{
+	ThreadSafeStack<std::string> stack;
+
+	stack.push("communist cat say mao mao");
+	stack.push("fox say ???");
+	stack.push("i say lalalalalalala");
+
+	stack.top() ? std::cout << *stack.top() << "\n" : std::cout << "Элемент не найден \n";
+	stack.pop();
+
+	stack.top() ? std::cout << *stack.top() << "\n" : std::cout << "Элемент не найден \n";
+	stack.pop();
+
+	stack.top() ? std::cout << *stack.top() << "\n" : std::cout << "Элемент не найден \n";
+	stack.pop();
+
+	/*std::thread t1([&stack, &str1]() { for (int i = 0; i < 30; ++i) { stack.push("communist cat say mao mao"); } });
+	std::thread t2([&stack, &str2]() { for (int i = 0; i < 30; ++i) { stack.push("fox say ???"); } });
+	std::thread t3([&stack, &str3]() { for (int i = 0; i < 30; ++i) { stack.push("i say lalalalalalala"); } });*/
+
+	
+}
+
